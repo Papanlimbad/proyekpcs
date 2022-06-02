eff07514bf204e3cffb5519ffe47fd1ce4ab2173
kurosunne <danielgamaliel06@gmail.com>
2022-06-02 17:41:08 +0700

    fix WebImage Class 2

--- Bukutachi/Bukutachi/WebImage.cs
+++ Bukutachi/Bukutachi/WebImage.cs
@@ -8,24 +8,31 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bukutachi {
-    class WebImage {
-        public static Image fromUrl(string url) {
+namespace Bukutachi
+{
+    class WebImage
+    {
+        public static Image fromUrl(string url)
+        {
             HttpWebRequest wreq = (HttpWebRequest)HttpWebRequest.Create(url);
 
-            using (HttpWebResponse wres = (HttpWebResponse)wreq.GetResponse()) {
-                using (Stream stream = wres.GetResponseStream()) {
+            using (HttpWebResponse wres = (HttpWebResponse)wreq.GetResponse())
+            {
+                using (Stream stream = wres.GetResponseStream())
+                {
                     return Image.FromStream(stream);
                 }
             }
         }
 
-        public static Image resizeImage(Image i, int nh) {
+        public static Image resizeImage(Image i, int nh)
+        {
             int h = nh;
             int w = (int)Math.Round(i.Width * 1.0 * nh / i.Height);
             Bitmap b = new Bitmap(w, h);
 
-            using (var graphic = Graphics.FromImage(b)) {
+            using (var graphic = Graphics.FromImage(b))
+            {
                 graphic.InterpolationMode = InterpolationMode.Default;
                 graphic.SmoothingMode = SmoothingMode.Default;
                 graphic.PixelOffsetMode = PixelOffsetMode.Default;
