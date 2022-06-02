881938980cf0858f28063e5a70bb3683532c7d9d
Soeg-Cronus <83352641+Soeg-Cronus@users.noreply.github.com>
2022-06-02 17:45:57 +0700

    Add Book Admin
    Edit form add book

--- Bukutachi/Bukutachi/FormAddBookAdmin.Designer.cs
+++ Bukutachi/Bukutachi/FormAddBookAdmin.Designer.cs
@@ -31,6 +31,12 @@ namespace Bukutachi
         {
             this.components = new System.ComponentModel.Container();
             this.lbSynopsis = new Guna.UI2.WinForms.Guna2Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.lbAddBook = new System.Windows.Forms.Label();
             this.tbSynopsis = new Guna.UI2.WinForms.Guna2TextBox();
             this.tbLocation = new Guna.UI2.WinForms.Guna2TextBox();
@@ -50,12 +56,6 @@ namespace Bukutachi
             this.btAddImage = new Guna.UI2.WinForms.Guna2Button();
             this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
             this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.lbSynopsis.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -91,6 +91,72 @@ namespace Bukutachi
             this.lbSynopsis.Name = "lbSynopsis";
             this.lbSynopsis.Size = new System.Drawing.Size(740, 535);
             this.lbSynopsis.TabIndex = 0;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(133, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 18);
+            this.label8.TabIndex = 40;
+            this.label8.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(133, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 18);
+            this.label7.TabIndex = 39;
+            this.label7.Text = ":";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(133, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 18);
+            this.label5.TabIndex = 38;
+            this.label5.Text = ":";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(133, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 18);
+            this.label3.TabIndex = 37;
+            this.label3.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(133, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 18);
+            this.label2.TabIndex = 36;
+            this.label2.Text = ":";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(133, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 18);
+            this.label1.TabIndex = 35;
+            this.label1.Text = ":";
             // 
             // lbAddBook
             // 
@@ -301,6 +367,7 @@ namespace Bukutachi
             this.btAddBook.TabIndex = 19;
             this.btAddBook.Tag = "0";
             this.btAddBook.Text = "Add Book";
+            this.btAddBook.Click += new System.EventHandler(this.btAddBook_Click);
             // 
             // label6
             // 
@@ -309,9 +376,9 @@ namespace Bukutachi
             this.label6.ForeColor = System.Drawing.Color.White;
             this.label6.Location = new System.Drawing.Point(19, 295);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(87, 18);
+            this.label6.Size = new System.Drawing.Size(127, 18);
             this.label6.TabIndex = 25;
-            this.label6.Text = "Synopsis :";
+            this.label6.Text = "Synopsis         :";
             // 
             // lbLocation
             // 
@@ -409,72 +476,6 @@ namespace Bukutachi
             // 
             this.guna2Elipse1.BorderRadius = 20;
             this.guna2Elipse1.TargetControl = this.lbSynopsis;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(133, 58);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(13, 18);
-            this.label1.TabIndex = 35;
-            this.label1.Text = ":";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(133, 96);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(13, 18);
-            this.label2.TabIndex = 36;
-            this.label2.Text = ":";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(133, 136);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(13, 18);
-            this.label3.TabIndex = 37;
-            this.label3.Text = ":";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(133, 175);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(13, 18);
-            this.label5.TabIndex = 38;
-            this.label5.Text = ":";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(133, 216);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(13, 18);
-            this.label7.TabIndex = 39;
-            this.label7.Text = ":";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(133, 254);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(13, 18);
-            this.label8.TabIndex = 40;
-            this.label8.Text = ":";
             // 
             // FormAddBookAdmin
             // 
--- Bukutachi/Bukutachi/FormAddBookAdmin.cs
+++ Bukutachi/Bukutachi/FormAddBookAdmin.cs
@@ -7,14 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace Bukutachi
 {
     public partial class FormAddBookAdmin : Form
     {
+        MySqlConnection conn;
+
         public FormAddBookAdmin()
         {
             InitializeComponent();
+            this.conn = conn;
+
+        }
+
+        private void btAddBook_Click(object sender, EventArgs e)
+        {
+            if (tbBookTitle.Text.Equals("") || tbAuthor.Text.Equals("") || tbGenre.Text.Equals("") || tbPublisher.Text.Equals("") || tbPublishDate.Text.Equals("") || tbLocation.Text.Equals(""))
+            {
+                MessageBox.Show("Field Harus Diisi Semua!");
+            }
+            else
+            {
+                MySqlCommand cmd = new MySqlCommand("insert into buku(bu_title, bu_synopsis, bu_publishedat, bu_status, bu_pt_id)");
+            }
         }
     }
 }
