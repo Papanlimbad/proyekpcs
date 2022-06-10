87613c746b12c1fd1794a411f5e7ed0f648f061c
mikhaelc09 <mikhael.chris09@gmail.com>
2022-06-10 14:05:28 +0700

    fix webimage class for no url case

--- Bukutachi/Bukutachi/WebImage.cs
+++ Bukutachi/Bukutachi/WebImage.cs
@@ -14,62 +14,74 @@ namespace Bukutachi
     {
         public static Image fromUrl(string url)
         {
-            HttpWebRequest wreq = (HttpWebRequest)HttpWebRequest.Create(url);
+            if(url.Length > 0) {
+                HttpWebRequest wreq = (HttpWebRequest)HttpWebRequest.Create(url);
 
-            using (HttpWebResponse wres = (HttpWebResponse)wreq.GetResponse())
-            {
-                using (Stream stream = wres.GetResponseStream())
+                using (HttpWebResponse wres = (HttpWebResponse)wreq.GetResponse())
                 {
-                    return Image.FromStream(stream);
+                    using (Stream stream = wres.GetResponseStream())
+                    {
+                        return Image.FromStream(stream);
+                    }
                 }
             }
+            return null;
         }
 
         public static Image resizeImage(Image i, int nw, int nh)
         {
-            Bitmap b = new Bitmap(nw, nh);
+            if(i != null) {
+                Bitmap b = new Bitmap(nw, nh);
 
-            using (var graphic = Graphics.FromImage(b))
-            {
-                graphic.InterpolationMode = InterpolationMode.Default;
-                graphic.SmoothingMode = SmoothingMode.None;
-                graphic.PixelOffsetMode = PixelOffsetMode.None;
-                graphic.CompositingQuality = CompositingQuality.HighSpeed;
-                graphic.DrawImage(i, 0, 0, nw, nh);
+                using (var graphic = Graphics.FromImage(b))
+                {
+                    graphic.InterpolationMode = InterpolationMode.Default;
+                    graphic.SmoothingMode = SmoothingMode.None;
+                    graphic.PixelOffsetMode = PixelOffsetMode.None;
+                    graphic.CompositingQuality = CompositingQuality.HighSpeed;
+                    graphic.DrawImage(i, 0, 0, nw, nh);
+                }
+                return b;
             }
-            return b;
+            return null;
         }
 
         public static Image resizeHeightImage(Image i, int nw) {
-            int w = nw;
-            int h = (int)Math.Round(nw * i.Height * 1.0 / i.Width);
-            Console.WriteLine($"{w} - {h}");
-            Bitmap b = new Bitmap(w, h);
+            if(i != null) {
+                int w = nw;
+                int h = (int)Math.Round(nw * i.Height * 1.0 / i.Width);
+                Console.WriteLine($"{w} - {h}");
+                Bitmap b = new Bitmap(w, h);
 
-            using (var graphic = Graphics.FromImage(b)) {
-                graphic.InterpolationMode = InterpolationMode.Default;
-                graphic.SmoothingMode = SmoothingMode.None;
-                graphic.PixelOffsetMode = PixelOffsetMode.None;
-                graphic.CompositingQuality = CompositingQuality.HighSpeed;
-                graphic.DrawImage(i, 0, 0, w, h);
+                using (var graphic = Graphics.FromImage(b)) {
+                    graphic.InterpolationMode = InterpolationMode.Default;
+                    graphic.SmoothingMode = SmoothingMode.None;
+                    graphic.PixelOffsetMode = PixelOffsetMode.None;
+                    graphic.CompositingQuality = CompositingQuality.HighSpeed;
+                    graphic.DrawImage(i, 0, 0, w, h);
+                }
+                return b;
             }
-            return b;
+            return null;
         }
 
         public static Image resizeWidthImage(Image i, int nh) {
-            int h = nh;
-            int w = (int)Math.Round(nh * i.Width * 1.0 / i.Height);
-            Console.WriteLine($"{w} - {h}");
-            Bitmap b = new Bitmap(w, h);
+            if(i != null) {
+                int h = nh;
+                int w = (int)Math.Round(nh * i.Width * 1.0 / i.Height);
+                Console.WriteLine($"{w} - {h}");
+                Bitmap b = new Bitmap(w, h);
 
-            using (var graphic = Graphics.FromImage(b)) {
-                graphic.InterpolationMode = InterpolationMode.Default;
-                graphic.SmoothingMode = SmoothingMode.None;
-                graphic.PixelOffsetMode = PixelOffsetMode.None;
-                graphic.CompositingQuality = CompositingQuality.HighSpeed;
-                graphic.DrawImage(i, 0, 0, w, h);
+                using (var graphic = Graphics.FromImage(b)) {
+                    graphic.InterpolationMode = InterpolationMode.Default;
+                    graphic.SmoothingMode = SmoothingMode.None;
+                    graphic.PixelOffsetMode = PixelOffsetMode.None;
+                    graphic.CompositingQuality = CompositingQuality.HighSpeed;
+                    graphic.DrawImage(i, 0, 0, w, h);
+                }
+                return b;
             }
-            return b;
+            return null;
         }
     }
 }
